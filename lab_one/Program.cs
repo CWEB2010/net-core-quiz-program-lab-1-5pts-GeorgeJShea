708af992ac580ceded6e8efb4a7d7bc0c3a401d0
Shea <shegeoj@dunwoody.edu>
2020-01-23 14:03:56 -0600

    version 2.0

--- lab_one/Program.cs
+++ lab_one/Program.cs
@@ -3,7 +3,7 @@
 _________________________________________________
 George Shea                          ßeta Product
 Created 22/1/2020
-Version 1.0
+Version 2.0
 Version Date: 22/1/2020
 
 small test that will test students on their knowledge
@@ -22,15 +22,14 @@ namespace ConsoleApp1
             string repeat = "Y";
             while (repeat == "Y")
             {
-                // this will see if the student is ready to test
-                // just a nice little intro screen
+                // intro screen teels you what the quiz is about
                 Console.WriteLine("Programming Fundementals Test");
                 Console.WriteLine("_________________________________________________");
                 Console.WriteLine("This test will quiz you on");
-                Console.WriteLine("on your knowledge of c#");
-                Console.WriteLine("please answer lower case");
+                Console.WriteLine("on your knowledge of .net");
+                Console.WriteLine("please answer in A,B,C or D");
                 Console.WriteLine("__________________________________________________");
-                Console.WriteLine("do you wish to begin y/n");
+                Console.WriteLine("Do You Wish To Begin y/n");
                 begin = Console.ReadLine();
 
                 if (begin == "n")
@@ -40,247 +39,89 @@ namespace ConsoleApp1
                 }
                 else
                 {
-
+                    // the great many of arrays needed to get this working
+                    // this stores what the correct answers to the quiz are
                     string[] testAswers = { "A", "B", "C", "A", "D",
                         "A", "A", "C", "D", "A" };
-                    string[] testQuess = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
-
+                    // this array will store the users gueses at the quiz it has 10 slots open
+                    // for the 10 questiosn that will be asked
+                    string[] testGuess = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+                    // this is just a personal touch
+                    //this is used to store all the questions to be printed out in a loop
+                    string[] testQuestions = {
+                        "Question 1 ", "A || ", "B || ", "C || ", "D || ",
+                        "Question 2 ", "A || ", "B || ", "C || ", "D || ",
+                        "Question 3 ", "A || ", "B || ", "C || ", "D || ",
+                        "Question 4 ", "A || ", "B || ", "C || ", "D || ",
+                        "Question 5 ", "A || ", "B || ", "C || ", "D || ",
+                        "Question 6 ", "A || ", "B || ", "C || ", "D || ",
+                        "Question 7 ", "A || ", "B || ", "C || ", "D || ",
+                        "Question 8 ", "A || ", "B || ", "C || ", "D || ",
+                        "Question 9 ", "A || ", "B || ", "C || ", "D || ",
+                        "Question 10 ", "A || ", "B || ", "C || ", "D || ",
+                    };
+                    //__________________________________________________________________________
+                    // this is used to pass/fail the person
                     int score = 0;
-                    // score is responsible for quiz grade
-                    // this will be quizes 
+                    // Qcount is responsible for going through the array and printing out the
+                    // correct words to make up the quiz
+                    int QCount = 0;
+                    // answers store the uses guesses temporality then puts it into the array
+                    string answers;
+                    // this moves the guesses into the correct questions
+                    int guessPositCount = 0;
+                    // just used to avoid having to loop through and make sure that it
+                    // does not get outside of its array 
+                    int qLoop = 10;
 
-                    //____________________________________________________________________________
-
-                    // takes user input and sees if it matches test answer
-                    // A correct answer
-
-                    string answerOne;
-
-                    Console.WriteLine("Question 1");
-                    Console.WriteLine(" A|| words");
-                    Console.WriteLine(" B|| words");
-                    Console.WriteLine(" C|| words");
-                    Console.WriteLine(" D|| words");
-                    answerOne = Console.ReadLine();
-                    answerOne = answerOne.ToUpper();
-                    if (answerOne == testAswers[0])
+                    // begin loop will loop 10 full times
+                    while (qLoop > 0)
                     {
-                        score = score + 1;
-                        testQuess[0] = answerOne;
+                        // prints out in groups of 5 1 Question 4 Solutions
+                        qLoop = qLoop - 1;
+                        Console.WriteLine(testQuestions[QCount]);
+                        QCount = QCount + 1;
+                        Console.WriteLine(testQuestions[QCount]);
+                        QCount = QCount + 1;
+                        Console.WriteLine(testQuestions[QCount]);
+                        QCount = QCount + 1;
+                        Console.WriteLine(testQuestions[QCount]);
+                        QCount = QCount + 1;
+                        Console.WriteLine(testQuestions[QCount]);
+                        QCount = QCount + 1;
 
-                    }
-                    else
-                    {
-                        testQuess[0] = answerOne;
-                    }
-                    Console.WriteLine();
-                    //_____________________________________________________________________________________
-                    // question 2
-                    string answerTwo;
 
-                    Console.WriteLine("Question 2");
-                    Console.WriteLine(" A|| words");
-                    Console.WriteLine(" B|| words");
-                    Console.WriteLine(" C|| words");
-                    Console.WriteLine(" D|| words");
-                    answerTwo = Console.ReadLine();
-                    answerTwo = answerTwo.ToUpper();
-                    if (answerTwo == testAswers[1])
-                    {
-                        score = score + 1;
-                        testQuess[1] = answerTwo;
+                        // gets user input and raies it to upper case
+                        // to avoid case senesitivity
+                        answers = Console.ReadLine();
+                        answers = answers.ToUpper();
+                        // if correct get a point and the guess is saved
+                        if (answers == testAswers[guessPositCount])
+                        {
+                            score = score + 1;
+                            testGuess[guessPositCount] = answers;
+                            guessPositCount = guessPositCount + 1;
 
+                        }
+                        // get it wrong and it still saves the guess
+                        else
+                        {
+                            testGuess[guessPositCount] = answers;
+                            guessPositCount = guessPositCount + 1;
+                        }
+                        // to keep test clean looking
+                        Console.WriteLine();
+                        Console.WriteLine();
                     }
-                    else
-                    {
-                        testQuess[1] = answerTwo;
-                    }
-                    Console.WriteLine();
-                    //_____________________________________________________________________________________
-                    // question 3
-                    string answerThree;
-
-                    Console.WriteLine("Question 3");
-                    Console.WriteLine(" A|| words");
-                    Console.WriteLine(" B|| words");
-                    Console.WriteLine(" C|| words");
-                    Console.WriteLine(" D|| words");
-                    answerThree = Console.ReadLine();
-                    answerThree = answerThree.ToUpper();
-                    if (answerThree == testAswers[2])
-                    {
-                        score = score + 1;
-                        testQuess[2] = answerThree;
-
-                    }
-                    else
-                    {
-                        testQuess[2] = answerThree;
-                    }
-                    Console.WriteLine();
-                    //_______________________________________________________________________________________
-                    // question 4
-                    string answerFour;
-
-                    Console.WriteLine("Question 4");
-                    Console.WriteLine(" A|| words");
-                    Console.WriteLine(" B|| words");
-                    Console.WriteLine(" C|| words");
-                    Console.WriteLine(" D|| words");
-                    answerFour = Console.ReadLine();
-                    answerFour = answerFour.ToUpper();
-                    if (answerFour == testAswers[3])
-                    {
-                        score = score + 1;
-                        testQuess[3] = answerFour;
-
-                    }
-                    else
-                    {
-                        testQuess[3] = answerFour;
-                    }
-                    Console.WriteLine();
-                    //_______________________________________________________________________________________
-                    // question 5
-                    string answerFive;
-
-                    Console.WriteLine("Question 5");
-                    Console.WriteLine(" A|| words");
-                    Console.WriteLine(" B|| words");
-                    Console.WriteLine(" C|| words");
-                    Console.WriteLine(" D|| words");
-                    answerFive = Console.ReadLine();
-                    answerFive = answerFive.ToUpper();
-                    if (answerFive == testAswers[4])
-                    {
-                        score = score + 1;
-                        testQuess[4] = answerFive;
-
-                    }
-                    else
-                    {
-                        testQuess[4] = answerFive;
-                    }
-                    Console.WriteLine();
-                    //_______________________________________________________________________________________
-                    // question 6
-                    string answerSix;
-
-                    Console.WriteLine("Question 6");
-                    Console.WriteLine(" A|| words");
-                    Console.WriteLine(" B|| words");
-                    Console.WriteLine(" C|| words");
-                    Console.WriteLine(" D|| words");
-                    answerSix = Console.ReadLine();
-                    answerSix = answerSix.ToUpper();
-                    if (answerSix == testAswers[5])
-                    {
-                        score = score + 1;
-                        testQuess[5] = answerSix;
-
-                    }
-                    else
-                    {
-                        testQuess[5] = answerSix;
-                    }
-                    Console.WriteLine();
-                    //_______________________________________________________________________________________
-                    // question 7
-                    string answerSeven;
-
-                    Console.WriteLine("Question 7");
-                    Console.WriteLine(" A|| words");
-                    Console.WriteLine(" B|| words");
-                    Console.WriteLine(" C|| words");
-                    Console.WriteLine(" D|| words");
-                    answerSeven = Console.ReadLine();
-                    answerSeven = answerSeven.ToUpper();
-                    if (answerSeven == testAswers[6])
-                    {
-                        score = score + 1;
-                        testQuess[6] = answerSeven;
-
-                    }
-                    else
-                    {
-                        testQuess[6] = answerSeven;
-                    }
-                    Console.WriteLine();
-                    //_______________________________________________________________________________________
-                    // question 8
-                    string answerEight;
-
-                    Console.WriteLine("Question 8");
-                    Console.WriteLine(" A|| words");
-                    Console.WriteLine(" B|| words");
-                    Console.WriteLine(" C|| words");
-                    Console.WriteLine(" D|| words");
-                    answerEight = Console.ReadLine();
-                    answerEight = answerEight.ToUpper();
-                    if (answerEight == testAswers[7])
-                    {
-                        score = score + 1;
-                        testQuess[7] = answerEight;
-
-                    }
-                    else
-                    {
-                        testQuess[7] = answerEight;
-                    }
-                    Console.WriteLine();
-                    //_______________________________________________________________________________________
-                    // question 9
-                    string answerNine;
-
-                    Console.WriteLine("Question 9");
-                    Console.WriteLine(" A|| words");
-                    Console.WriteLine(" B|| words");
-                    Console.WriteLine(" C|| words");
-                    Console.WriteLine(" D|| words");
-                    answerNine = Console.ReadLine();
-                    answerNine = answerNine.ToUpper();
-                    if (answerNine == testAswers[8])
-                    {
-                        score = score + 1;
-                        testQuess[8] = answerNine;
-
-                    }
-                    else
-                    {
-                        testQuess[8] = answerNine;
-                    }
-                    Console.WriteLine();
-                    //_______________________________________________________________________________________
-                    // question 10 yea
-                    string answerTen;
-
-                    Console.WriteLine("Question 10");
-                    Console.WriteLine(" A|| words");
-                    Console.WriteLine(" B|| words");
-                    Console.WriteLine(" C|| words");
-                    Console.WriteLine(" D|| words");
-                    answerTen = Console.ReadLine();
-                    answerTen = answerTen.ToUpper();
-                    if (answerTen == testAswers[9])
-                    {
-                        score = score + 1;
-                        testQuess[9] = answerTen;
-
-                    }
-                    else
-                    {
-                        testQuess[9] = answerTen;
-                    }
-                    Console.WriteLine();
-                    //____________________________________________________
-                    // score screen
                     Console.WriteLine();
                     Console.WriteLine("Test Completed");
                     Console.WriteLine();
                     Console.WriteLine();
                     Console.WriteLine("__________________________________________________________");
+                    // used to go through the array
                     int count = 0;
                     Console.WriteLine("Correct Answers: ");
+                    // prints out acutal answers to test
                     foreach (string name in testAswers)
                     {
                         if (count == 0)
@@ -294,19 +135,21 @@ namespace ConsoleApp1
                             count = count + 1;
                         }
                     }
+                    //-------------------------------------------
                     count = 0;
                     Console.WriteLine();
                     Console.WriteLine("Your Answers:");
-                    foreach (string name in testQuess)
+                    // prints out what the users guessed
+                    foreach (string name in testGuess)
                     {
                         if (count == 0)
                         {
-                            Console.Write(" " + testQuess[count]);
+                            Console.Write(" " + testGuess[count]);
                             count = count + 1;
                         }
                         else
                         {
-                            Console.Write(", " + testQuess[count]);
+                            Console.Write(", " + testGuess[count]);
                             count = count + 1;
                         }
                     }
@@ -314,6 +157,7 @@ namespace ConsoleApp1
                     Console.WriteLine();
                     Console.WriteLine();
 
+                    // just checks if you passed or failed
                     Console.WriteLine("Your Finale Score is " + score);
                     if (score >= 7)
                     {
@@ -326,6 +170,7 @@ namespace ConsoleApp1
                         Console.WriteLine("Sorry You Failed Free To Try Again");
                         Console.WriteLine();
                     }
+                    // will loop again so you can attempt the test again
                     Console.WriteLine("Press Y To Do The Test Again N to stop");
                     repeat = Console.ReadLine();
                     repeat = repeat.ToUpper();
